7c56b88290f16ce6c3bd96eaf034244675b976f7
eliyaoo32 <eliyaoo32@gmail.com>
2020-08-18 14:24:47 +0300

    Change icon of none player color

--- src/components/GameInfo/PlayerColor.js
+++ src/components/GameInfo/PlayerColor.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 const PlayerColor = ({ color }) => {
     const style = {
-        background: color,
+        background: color || '#cccccc',
+        font: '16px bold',
+        color: 'red',
+        textAlign: 'center',
         width: '20px',
         height: '20px',
         border: '1px solid #000'
     };
+
     return (
-        <div style={style}></div>
+    <div style={style}>{color === null && <>X</>}</div>
     );
 };
 
